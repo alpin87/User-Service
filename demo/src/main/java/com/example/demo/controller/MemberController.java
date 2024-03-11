5144de6ff8d7ba13130a0019599ec92c77eb8abf
alpin87 <qortmdals94@naver.com>
2024-03-11 18:00:51 +0900

    240311 html 수정완료 index.html삭제해둠

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,16 +1,14 @@
 package com.example.demo.controller;
 
-
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-
     private final MemberService memberService;
 
-    @Autowired
+    @Autowired  // 생성자 주입 방식
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
